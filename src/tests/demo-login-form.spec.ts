2415c899a82f92aaeb50f8302bb75c5d44ba03eb
y.kleshchar <y.kleshchar@favbet.tech>
2025-10-19 20:24:17 +0300

    hw-21

--- src/tests/demo-login-form.spec.ts
+++ src/tests/demo-login-form.spec.ts
@@ -1,8 +1,5 @@
 import { test, expect } from '@playwright/test';
-
-enum NOTIFICATIONS {
-  SUCCESS_REGISTER = 'Successfully registered! Please, click Back to return on login page',
-}
+import { invalidData, NOTIFICATIONS } from 'data/demo-login-form/register.data';
 
 test.describe('[DEMO REGISTER PAGE][SMOKE] Registration', () => {
   const baseUrl = 'https://anatoly-karpovich.github.io/demo-login-form/';
@@ -68,4 +65,43 @@ test.describe('[DEMO REGISTER PAGE][SMOKE] Registration', () => {
     await backButton.click();
     await expect(loginFormText, '[ASSERT] Verify return to login page').toHaveText('Login');
   });
+});
+
+test.describe('[DEMO REGISTER PAGE][NEGATIVE] Registration', () => {
+  /**
+    * Создать тест сьют используя DDT подход с негативными тест-кейсами по регистрации на сайте
+      https://anatoly-karpovich.github.io/demo-login-form/
+
+    Требования:
+    Страница регистрации:
+      Username: обязательное, от 3 до 40 символов включительно, запрещены префиксные/постфиксные пробелы, как и имя состоящее из одних пробелов
+      Password: обязательное, от 8 до 20 символов включительно, необходима хотя бы одна буква в верхнем и нижнем регистрах, пароль из одних пробелов запрещен
+  */
+
+  const baseUrl = 'https://anatoly-karpovich.github.io/demo-login-form/';
+
+  test.beforeEach(async ({ page }) => {
+    const loginFormText = page.locator('//h2[@id="loginForm"]');
+    const registerFormText = page.locator('//h2[@id="registerForm"]');
+    const registerButtonOnLoginPage = page.locator('//input[@id="registerOnLogin"]');
+
+    await page.goto(baseUrl);
+    await expect.soft(loginFormText, '[ASSERT] Verify login page').toHaveText('Login');
+    await registerButtonOnLoginPage.click();
+    await expect.soft(registerFormText, '[ASSERT] Verify register form').toHaveText('Registration');
+  });
+
+  for (const { title, credentials, errorMessage } of invalidData) {
+    test(`${title}`, async ({ page }) => {
+      const usernameRegisterInputLocator = page.locator('//input[@id="userNameOnRegister"]');
+      const passwordRegisterInputLocator = page.locator('//input[@id="passwordOnRegister"]');
+      const registerButtonLocator = page.locator('//input[@id="register"]');
+      const errorMessageLocator = page.locator('//h4[@id="errorMessageOnRegister"]');
+
+      await usernameRegisterInputLocator.fill(credentials.username);
+      await passwordRegisterInputLocator.fill(credentials.password);
+      await registerButtonLocator.click();
+      await expect(errorMessageLocator, `[ASSERT] Verify error message is ${errorMessage}`).toHaveText(errorMessage);
+    });
+  }
 });
---
+++ src/tests/table-rows.spec.ts
@@ -0,0 +1,58 @@
+/**
+ * Создать функцию getTableRow(page, email), которая возвращает строку в таблице по емейлу.
+Например getTableRow(page, 'jsmith@gmail.com') => { "Last Name": "Smith", "First Name": "John", Email: "jsmith@gmail.com", Due: "$50.00", "Web Site": "http://www.jsmith.com" }
+
+Создайте тест, проверяющий данную функцию, используя все емейлы из таблицы Example 2
+
+Сайт: https://the-internet.herokuapp.com/tables
+*/
+
+import { expect, Page, test } from '@playwright/test';
+import { IUser, listOfUsers } from 'data/table-rows/table-rows.data';
+
+test.describe('[Heroku App] Table', () => {
+  const baseUrl = 'https://the-internet.herokuapp.com/tables';
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(baseUrl);
+    const table = page.locator('#table2');
+    expect(table).toBeVisible();
+  });
+
+  for (const user of listOfUsers) {
+    test(`Find user by email ${user.Email}`, async ({ page }) => {
+      const expectedUser = await getTableRow({ page, email: user.Email });
+      expect(expectedUser, '[ASSERT] The user found matches the expected user').toEqual(user);
+    });
+  }
+});
+
+async function getTableRow(data: { page: Page; email: string }): Promise<IUser | string> {
+  const { page, email } = data;
+  const table = page.locator('#table2');
+
+  const headers = await table.locator('th').allInnerTexts();
+  headers.pop();
+
+  const tableRows = await table.locator('tbody tr').all();
+  const usersData: Record<string, string>[] = [];
+  for (const row of tableRows) {
+    const cells = await row
+      .locator('td')
+      .filter({ hasNot: page.locator('a') })
+      .allInnerTexts();
+
+    const rowData = headers.reduce(
+      (acc, el, i) => {
+        acc[el] = cells[i]!;
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+
+    usersData.push(rowData);
+  }
+
+  const expectedUser: IUser = usersData.find((user) => user.Email === email) as unknown as IUser;
+  return expectedUser ? expectedUser : 'No user with this email was found';
+}
