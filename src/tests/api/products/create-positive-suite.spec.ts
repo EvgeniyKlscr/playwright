b240283a94d63af90091f95dd13527cfc69c3a33
y.kleshchar <y.kleshchar@favbet.tech>
2025-11-15 17:07:16 +0200

    fix api tests

--- src/tests/api/products/create-positive-suite.spec.ts
+++ src/tests/api/products/create-positive-suite.spec.ts
@@ -45,7 +45,9 @@ test.describe('[API] [Sales Portal] [Products]', () => {
       id = createdProduct.body.Product._id;
 
       const actualProductData = createdProduct.body.Product;
-      expect(_.omit(actualProductData, ['_id', 'createdOn'])).toEqual(createdProduct);
+      expect(_.omit(actualProductData, ['_id', 'createdOn'])).toEqual(
+        _.omit(createdProduct.body.Product, ['_id', 'createdOn']),
+      );
     });
   }
 });
